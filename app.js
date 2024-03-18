1d3970527e4d4fbe182b7dadadd94bef6af7a39d
Adam Grodzki <110930032+AdamGrodzki@users.noreply.github.com>
2024-03-18 15:08:17 +0100

    add pop-up details for every pokemon on the page

--- app.js
+++ app.js
@@ -1,56 +1,57 @@
 const pokemonGallery = document.getElementById("pokemon-gallery");
+const pagesContainer = document.getElementById("pages-container");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
+const modal = document.getElementById("modal");
+const close = document.getElementById("close");
 
+const apiUrl = "https://pokeapi.co/api/v2/pokemon";
 
-const loader = document.getElementById("loading");
+const colors = {
+  normal: '#A8A77A',
+  fire: '#EE8130',
+  water: '#6390F0',
+  electric: '#F7D02C',
+  grass: '#7AC74C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  bug: '#A6B91A',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dragon: '#6F35FC',
+  dark: '#705746',
+  steel: '#B7B7CE',
+  fairy: '#D685AD',
+};
 
+const mainTypes = Object.keys(colors);
 
-let limit = 19;
 let offset = 1;
+let limit = 19;
+const pokemonPerPage = 20;
 
-
-
-prevBtn.addEventListener('click', ()=> {
-  if( offset > 1){
-    offset -=20;
+prevBtn.addEventListener('click', () => {
+  if (offset > 1) {
+    offset -= 20;
     removeChildNodes(pokemonGallery);
     fetchPokemons(offset, limit)
   }
 });
 
-nextBtn.addEventListener('click', ()=> {
+nextBtn.addEventListener('click', () => {
   offset += 20;
   removeChildNodes(pokemonGallery)
   fetchPokemons(offset, limit)
 });
 
-const colors = {
-	normal: '#A8A77A',
-	fire: '#EE8130',
-	water: '#6390F0',
-	electric: '#F7D02C',
-	grass: '#7AC74C',
-	ice: '#96D9D6',
-	fighting: '#C22E28',
-	poison: '#A33EA1',
-	ground: '#E2BF65',
-	flying: '#A98FF3',
-	psychic: '#F95587',
-	bug: '#A6B91A',
-	rock: '#B6A136',
-	ghost: '#735797',
-	dragon: '#6F35FC',
-	dark: '#705746',
-	steel: '#B7B7CE',
-	fairy: '#D685AD',
-};
 
-const mainTypes = Object.keys(colors);
 
 const fetchPokemons = async () => {
-  loader.style.display = "block";
-  for (let i = offset; i <=offset + limit; i++) {
+  for (let i = offset; i <= offset + limit; i++) {
     await getPokemon(i);
   }
 };
@@ -59,125 +60,112 @@ const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  loader.style.display = "none";
-    createPokemonCard(data)
+  createPokemonCard(data)
 };
 
 
-function removeChildNodes(parent) {
+const removeChildNodes = (parent) => {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
 }
 
-
-
-const pagesContainer = document.getElementById("pages-container")
-const pokemonPerPage = 20;
-const apiUrl = "https://pokeapi.co/api/v2/pokemon";
-
-const pokeRequest = ()=>{
-fetchPokemons(offset, pokemonPerPage);
+const pokeRequest = () => {
+  fetchPokemons(offset, pokemonPerPage);
 
   fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data)=>  {
+    .then((response) => response.json())
+    .then((data) => {
 
-    pagesContainer.innerHTML = "";
-    totalPages= Math.ceil(data.count/pokemonPerPage); 
-    let currentPage = Math.ceil(offset/pokemonPerPage);
+      pagesContainer.innerHTML = "";
+      // totalPages = Math.ceil(data.count / pokemonPerPage);
+      totalPages = Math.ceil(151 / pokemonPerPage);
+      let currentPage = Math.ceil(offset / pokemonPerPage);
 
-    for ( let i=1; i<= totalPages; i++){
-      const pageBtn = document.createElement("button")
-      pageBtn.textContent = i;
-      
-      pageBtn.addEventListener("click", function () {
-        offset = (i-1)*pokemonPerPage+1;
-        removeChildNodes(pokemonGallery);
-        pokeRequest(`${apiUrl}"offset="${offset}"&limit=20"`)
-      })
+      for (let i = 1; i <= totalPages; i++) {
+        const pageBtn = document.createElement("button")
+        pageBtn.textContent = i;
 
-      if(i ==  currentPage){
-        //make button active only when user is on that page 
-        pageBtn.classList.add("active")
+        pageBtn.addEventListener("click", function () {
+          offset = (i - 1) * pokemonPerPage + 1;
+          removeChildNodes(pokemonGallery);
+          pokeRequest(`${apiUrl}"offset="${offset}"&limit=20"`)
+        })
+        if (i == currentPage) {
+          //make button active only when user is on that page 
+          pageBtn.classList.add("active")
+        }
+        pagesContainer.appendChild(pageBtn)
       }
-      
-      pagesContainer.appendChild(pageBtn)
-    }
-  })
-
+      console.log("data:", data)
+    })
 }
-
 pokeRequest();
-
 
 const createPokemonCard = (pokemon) => {
   const pokemonEl = document.createElement("div");
-  const pokemonElBack = document.createElement('div');
   pokemonEl.classList.add("pokemon");
-  // console.log(pokemon)
 
   const name = pokemon.name.toUpperCase();
-  const img = pokemon.sprites.front_default;
-  const id = pokemon.id.toString().padStart(3, 0);
-  const height = (pokemon.height/10).toFixed(1) + "m";
-  const weight = (pokemon.weight/10) + "kg";
-  const pokeTypes = pokemon.types.map((type) => type.type.name).join(", ");
-  const type = mainTypes.find((type) => pokeTypes.indexOf(type) >-1);
+  const pokeTypes = pokemon.types.map((type) => type.type.name);
+  const type = mainTypes.find((type) => pokeTypes.indexOf(type) > -1);
   const color = colors[type];
 
   pokemonEl.style.backgroundColor = color;
-  pokemonElBack.style.backgroundColor = color;
-
-  // const pokemonInnerHTML = `
-  //   <div class="img-container">
-  //       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${name}">
-  //   </div>
-  //   <div class="info">
-  //       <span class="number">#${id}</span>
-  //       <h3 class="name">${name}</h3>
-  //       <p class="height">Height: ${height}<p>
-  //       <p class="weight">Weight: ${weight}</p>
-  //       <p class="type">Type: <span>${type}</span> </p>
-  //   </div>
-  //   `;
-  // pokemonEl.innerHTML = pokemonInnerHTML;
 
   const pokemonFrontEl = document.createElement('div');
-  pokemonFrontEl.classList.add('front');
   pokemonFrontEl.innerHTML = `
-    <div class ="info">
-    <h3 class="name">${name} </h3>
-    </div>
-    `;
-  pokemonEl.appendChild(pokemonFrontEl);
-
-  const pokemonBackEl = document.createElement('div');
-  pokemonBackEl.classList.add('back');
-  pokemonBackEl.classList.add('flipped');
-  pokemonBackEl.innerHTML = `
-  <p class="idPoke">#${id}</p>
-  <div class="img-container">
-      <img src="${img}" alt="${name}">
-  </div>
-  <div class="stats">
-    <p class="height">Height: ${height}<p>
-    <p class="weight">Weight: ${weight}</p>
-    <p class="type">Type: <span>${type}</span> </p>
-    </div>
+  <p class="name">${name} </p>
   `;
 
-  pokemonEl.appendChild(pokemonBackEl);
-
+  pokemonEl.appendChild(pokemonFrontEl);
 
   pokemonGallery.appendChild(pokemonEl);
 
-  function flipCard() {
-    const p = this.closest('.pokemon');
-    p.querySelector('.front').classList.toggle('flipped');
-    p.querySelector('.back').classList.toggle('flipped');
+  const closePokemonDetails = () => {
+    modal.style.display = "none"
   }
-  pokemonFrontEl.addEventListener("click", flipCard);
-  pokemonBackEl.addEventListener("click", flipCard);
+  close.addEventListener("click", closePokemonDetails)
 
+  pokemonFrontEl.addEventListener("click", () => {
+    showPokemonDetails(pokemon)
+    modal.style.display = "flex"
+  });
+
+  console.log("offset: ", offset)
+  if (offset == 1) {
+    prevBtn.classList.add('hidden')
+  } else {
+    prevBtn.classList.remove('hidden')
+  }
+
+  if (offset == 141) {
+    nextBtn.classList.add('hidden')
+  } else {
+    nextBtn.classList.remove('hidden')
+  }
 }
+
+const showPokemonDetails = (data) => {
+  const titleModal = document.getElementById("modal-title");
+  const imgModal = document.getElementById('modal-img');
+  const heightModal = document.getElementById('modal-height');
+  const weightModal = document.getElementById('modal-weight');
+  const typeModal = document.getElementById('modal-type');
+  const pokeTypes = data.types.map((type) => type.type.name);
+
+  titleModal.textContent = `#${data.id.toString().padStart(3, 0)} ${capitalize(data.name)}`;
+  imgModal.src = data.sprites.front_default;
+  heightModal.textContent = (data.height / 10).toFixed(1) + "m";
+  weightModal.textContent = (data.weight / 10) + "kg";
+  typeModal.textContent = capitalize(mainTypes.find((type) => pokeTypes.indexOf(type) > -1));
+
+
+  modal.classList.add('show')
+  console.log("Show: ")
+}
+
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
